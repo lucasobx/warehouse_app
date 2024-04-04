32a6c29a2a0780796417ea9b3a545801ff75026e
Lucas <obxlucas@gmail.com>
2024-04-04 20:38:04 -0300

    Voltar para a tela inicial

---
+++ spec/system/user_view_warehouse_details_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+describe 'Usuário vê detalhes de um galpão' do
+  it 'e vê informação adicionais' do
+    Warehouse.create(name: 'Aeroporto SP', code: 'GRU', city: 'Guarulhos', area: 100_000,
+                     address: 'Avenida do Aeroporto, 1000', cep: '15000-000',
+                     description: 'Galpão destinado para cargas internacionais')
+
+    visit root_path
+    click_on 'Aeroporto SP'
+
+    expect(page).to have_content 'Galpão GRU'
+    expect(page).to have_content 'Nome: Aeroporto SP'
+    expect(page).to have_content 'Cidade: Guarulhos'
+    expect(page).to have_content 'Área: 100000 m2'
+    expect(page).to have_content 'Endereço: Avenida do Aeroporto, 1000 CEP: 15000-000'
+    expect(page).to have_content 'Galpão destinado para cargas internacionais'
+  end
+
+  it 'e volta para a tela inicial' do
+    Warehouse.create(name: 'Aeroporto SP', code: 'GRU', city: 'Guarulhos', area: 100_000,
+                     address: 'Avenida do Aeroporto, 1000', cep: '15000-000',
+                     description: 'Galpão destinado para cargas internacionais')
+
+    visit root_path
+    click_on 'Aeroporto SP'
+    click_on 'Voltar'
+
+    expect(current_path).to eq root_path
+  end
+end
