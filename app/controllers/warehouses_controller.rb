40354f6f07433f4309540c561972fb5cc7013822
Lucas <obxlucas@gmail.com>
2024-04-06 16:48:30 -0300

    Adiciona flash messages

--- app/controllers/warehouses_controller.rb
+++ app/controllers/warehouses_controller.rb
@@ -2,4 +2,19 @@ class WarehousesController < ApplicationController
   def show
     @warehouse = Warehouse.find(params[:id])
   end
+
+  def new
+    @warehouse = Warehouse.new
+  end
+
+  def create
+    @warehouse = Warehouse.new(warehouse_params)
+    @warehouse.save
+    redirect_to root_path, notice: "Galpão cadastrado com sucesso."
+  end
+
+  private
+  def warehouse_params
+    params.require(:warehouse).permit(:name, :code, :city, :area, :address, :cep, :description)
+  end
 end
