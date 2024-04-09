fdd7a2d85e514b1ddd418067192bb82d0ef643ec
Lucas <obxlucas@gmail.com>
2024-04-09 15:48:37 -0300

    Usuário vê modelo de produto

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'home#index'
-  resources 'warehouses', except: [:index]
-  resources 'suppliers', except: [:destroy]
+  resources :warehouses, except: [:index]
+  resources :suppliers, except: [:destroy]
+  resources :product_models, only: [:index]
 end
