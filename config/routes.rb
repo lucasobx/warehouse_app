5615215d19c4417ef76cc93b440eed12a0f40b4f
Lucas <obxlucas@gmail.com>
2024-04-19 22:31:59 -0300

    refactor: refatora rotas

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'home#index'
-  resources :warehouses, except: [:index]
-  resources :suppliers, except: [:destroy]
-  resources :product_models, except: [:destroy]
+  resources :warehouses, only: [:show, :new, :create, :edit, :update, :destroy]
+  resources :suppliers, only: [:index, :show, :new, :create, :edit, :update]
+  resources :product_models, only: [:index, :show, :new, :create, :edit, :update]
   resources :orders, only: [:show, :new, :create]
 end
